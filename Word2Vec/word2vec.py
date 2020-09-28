1afe4f2511ac4c6a91f617068161ec6ec79093bc
Lee Geonhee <rjsgmlsms126@naver.com>
2020-09-29 07:08:36 +0900

    Update word2vec.py

--- Word2Vec/word2vec.py
+++ Word2Vec/word2vec.py
@@ -1,1 +1,43 @@
-z
+import collections
+import math
+import os
+import zipfile
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+
+# step1
+url = 'http://mattmahoney.net/dc/'
+
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception('Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+filename = maybe_download('text8.zip', 31344016)
+
+
+def read_data(filename):
+    ''' zip 파일에 포함된 텍스트 파일을 읽어서 단어 리스트 생성. 포함된 파일은 1개. 
+    zip 파일은 30mb, txt 파일은 100mb. '''
+    with zipfile.ZipFile(filename) as f:
+        names = f.namelist()                # ['text8']
+        contents = f.read(names[0])         # 크기 : 100,000,000바이트
+        text = tf.compat.as_str(contents)   # 크기 : 100,000,000
+        return text.split()                 # 갯수 : 17005207
+
+
+vocabulary = read_data(filename)
+print('Data size', len(vocabulary))         # 17005207
+
+
+
+#step2
